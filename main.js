8d340178d88a59a3d9ed870acdb0d796d2bedff1
nikhil-babu0707 <87303388+nikhil-babu0707@users.noreply.github.com>
2022-01-14 13:20:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-function preload() {
+noseX = 0;
+noseY = 0;
 
+
+function preload() {
+    clown_nose = loadImage('https://i.postimg.cc/JhnVt1M1/clown.png');
 }
 
 function setup() {
@@ -14,14 +18,18 @@ function setup() {
 
 function draw() {
     image(video, 0, 0, 300, 300);
+    image(clown_nose, noseX, noseY, 30, 30);
 }
 
 
 function gotPoses(results) {
     if (results.length > 0) {
         console.log(results);
-        console.log("nose x = " + results[0].pose.nose.x);
-        console.log("nose y = " + results[0].pose.nose.y);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
+
     }
 }
 
